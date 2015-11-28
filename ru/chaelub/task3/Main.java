41b89e21448a3e1469d25f8be2fac7e5c0208390
Vadim Kravchenko <chaelub@gmail.com>
2015-11-28 17:01:13 +0300

    Hmmmm. Hrrr. Added default values for start/end/step in main, because otherwice not compiled.

--- ru/chaelub/task3/Main.java
+++ ru/chaelub/task3/Main.java
@@ -15,15 +15,33 @@ public class Main {
     }
   System.out.println("Welcom!");
 
-  String startDiapasonS = c.readLine("Please, define the diapson start:");
-  String endDiapasonS = c.readLine("Please, define the diapson end:");
-  String stepS = c.readLine("And please, define the step:");
 
-  double startDiapason = Double.valueOf(startDiapasonS);
-  double endDiapason = Double.valueOf(endDiapasonS);
-  double step = Double.valueOf(stepS);
+  String startDiapasonS;
+  String endDiapasonS;
+  String stepS;
+  double startDiapason=0.0;
+  double endDiapason=10.0;
+  double step=1.0;
+  boolean ok = false;
+  while(!ok){
+    startDiapasonS = c.readLine("Please, define the diapson start:");
+    endDiapasonS = c.readLine("Please, define the diapson end:");
+    stepS = c.readLine("And please, define the step:");
+    startDiapason = Double.valueOf(startDiapasonS);
+    endDiapason = Double.valueOf(endDiapasonS);
+    step = Double.valueOf(stepS);
+    if((startDiapason > endDiapason)||(step<=0)){
+      System.out.println("Please input correct values. All values must be double;\n'Diapason Start' must be less then 'Diapson End';\n'Step' must be greater than 0 (zero).");
+    }
+    else{
+      ok=true;
+    }
 
-  Fun f = new Fun(startDiapason, endDiapason, step);
-  f.calculateFun();
+  }
+
+  if(ok){
+    Fun f = new Fun(startDiapason, endDiapason, step);
+    f.calculateFun();
+  }
   }
 }
